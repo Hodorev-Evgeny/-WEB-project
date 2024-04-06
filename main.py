3eb61be7947e6068ed4ac83933a57e354d4fa4b0
User Evgeny <hodorevevgenij6@gmail.com>
2024-04-06 15:29:42 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import flask
+from data import db_session
+
+app = flask.Flask(__name__)
+app.secret_key = 'secret'
+
+
+@app.route('/')
+def index():
+    return flask.render_template('chat.html')
+
+
+@app.route('/home'):
+def home():
+
+
+
+if __name__ == '__main__':
+    db_session.global_init('db/data.db')
+    app.run('', port=5000, debug=True)
+
